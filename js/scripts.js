adc9bad1c5916b3242cf0624bad1210f2a4e77b6
Shirish Veerabattini <shirish101k@gmail.com>
2021-07-08 23:00:28 -0500

    updated scripts and invite code

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,10 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '94a21ee6e1322ec4bd6fdecf1d4fed76') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbz5uk9-7rBFfjwSbd2oekaiScM4d2MZpIKs6WpoWpqgJmqepPi3XT2N2Fj89rNIBO2-/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
